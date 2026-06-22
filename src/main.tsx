29ba9c703590e5268bf70543b5c748f62f87d5a2
AmineSeRaFimmm <eduardowgx@gmail.com>
2026-06-22 09:52:22 +0800

    Load progress tracker styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StandaloneProgressTracker } from "./components/StandaloneProgressTracker";
 import "./styles/global.css";
+import "./styles/progress.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
