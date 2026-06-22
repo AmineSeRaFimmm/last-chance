9d4b5987a6d66871bf56bf33736eb5e85e2323d5
AmineSeRaFimmm <eduardowgx@gmail.com>
2026-06-22 20:07:19 +0800

    Install timeline control

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import Root from "./Root";
 import { installNumericInputGuard } from "./core/numericInputGuard";
 import { installProjectionWheel } from "./core/projectionWheel";
+import { installTimelineControl } from "./core/timelineControl";
 import "./styles/global.css";
 import "./styles/progress.css";
 import "./styles/navigation.css";
 import "./styles/projection.css";
 import "./styles/diet.css";
+import "./styles/timeline.css";
 
 installNumericInputGuard();
 installProjectionWheel();
+installTimelineControl();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
