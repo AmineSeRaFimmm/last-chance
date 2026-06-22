1c6d45f53aa28c74476f835f9c531a1db0c3138a
AmineSeRaFimmm <eduardowgx@gmail.com>
2026-06-22 10:41:26 +0800

    Install numeric input guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
+import { installNumericInputGuard } from "./core/numericInputGuard";
 import "./styles/global.css";
 import "./styles/progress.css";
 import "./styles/navigation.css";
 import "./styles/projection.css";
+
+installNumericInputGuard();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
